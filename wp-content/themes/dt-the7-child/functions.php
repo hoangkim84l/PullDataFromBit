1598abd0d799c31281291dd262b65c04b2c9b58c
VPhuongDuy <vd@embrace-it.com>
2020-04-29 16:45:22 +0700

    Adding dummy data content homepage and about us page

--- wp-content/themes/dt-the7-child/functions.php
+++ wp-content/themes/dt-the7-child/functions.php
@@ -4,23 +4,23 @@
  *
  */
 /*Add custom js code*/
-//add new class when user not login
 add_action('wp_footer', 'check_footer_bar');
 function check_footer_bar()
 {
-  global $wp;
+  $hURL =  get_home_url();
   ?>
     <div id="img-foo-bar">
         <p>
-            <img src="wp-content/uploads/2020/04/stripe.png" alt="" class="img-footerbar" height="50">
-            <img src="wp-content/uploads/2020/04/mpesa.png" alt="" class="img-footerbar" height="50">
+            <img src="<?php echo $hURL ?>/wp-content/uploads/2020/04/stripe.png" alt="" class="img-footerbar" height="50">
+            <img src="<?php echo $hURL ?>/wp-content/uploads/2020/04/mpesa.png" alt="" class="img-footerbar" height="50">
         </p>
     </div>
-<script type='text/javascript'>
-  jQuery(document).ready(function() {
-      var imgFooter = document.getElementById("img-foo-bar");
-      jQuery(".wf-float-right").append(imgFooter);
-  });
-</script>
+    <script type='text/javascript'>
+      jQuery(document).ready(function() {
+        var fileName = location.pathname.split("/");
+          var imgFooter = document.getElementById("img-foo-bar");
+          jQuery(".wf-float-right").append(imgFooter);
+      });
+    </script>
 <?php
 }

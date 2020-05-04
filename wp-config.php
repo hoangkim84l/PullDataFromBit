49845036d641eba442ae7bafc6f4be3b47241f59
VPhuongDuy <vd@embrace-it.com>
2020-05-04 17:07:34 +0700

    adding dummy content all page

--- wp-config.php
+++ wp-config.php
@@ -17,7 +17,7 @@
  *
  * @package WordPress
  */
-
+ini_set("memory_limit", -1);
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
 define( 'DB_NAME', 'mpaygwebstore' );
--- wp-content/themes/dt-the7-child/functions.php
+++ wp-content/themes/dt-the7-child/functions.php
@@ -18,8 +18,20 @@ function check_footer_bar()
     <script type='text/javascript'>
       jQuery(document).ready(function() {
         var fileName = location.pathname.split("/");
-          var imgFooter = document.getElementById("img-foo-bar");
-          jQuery(".wf-float-right").append(imgFooter);
+        var imgFooter = document.getElementById("img-foo-bar");
+        jQuery(".wf-float-right").append(imgFooter);
+        
+        jQuery( "h1:contains('M-PayG')" ).css( "text-transform", "uppercase" );
+        jQuery( "h2:contains('M-PayG')" ).css( "text-transform", "uppercase" );
+        jQuery( "h3:contains('M-PayG')" ).css( "text-transform", "uppercase" );
+        jQuery( "h4:contains('M-PayG')" ).css( "text-transform", "uppercase" );
+        jQuery( "h5:contains('M-PayG')" ).css( "text-transform", "uppercase" );
+        jQuery( "h6:contains('M-PayG')" ).css( "text-transform", "uppercase" );
+        jQuery( "span:contains('M-PayG')" ).css( "text-transform", "uppercase" );
+        jQuery( "p:contains('M-PayG')" ).css( "text-transform", "uppercase" );
+        jQuery( "a:contains('M-PayG')" ).css( "text-transform", "uppercase" );
+        var span = jQuery("#main");
+        span.html(span.html().replace(/M-PayG/, '<span style="text-transform: uppercase">$&</span>'));
       });
     </script>
 <?php
